c1c86f27e33f69560b7cf5416ac0dc8c5f32aa0c
adilw3nomad <adiln1993@googlemail.com>
2018-11-08 03:10:28 +0000

    Added spec for crypto ticker model

---
+++ spec/models/crypto_ticker_spec.rb
@@ -0,0 +1,19 @@
+require_relative '../spec_helper'
+
+describe CryptoTicker do
+  let(:bitcoin_gbp_ticker){ CryptoTicker.new(id: 1, currency: 'GBP')}
+  before do 
+    VCR.insert_cassette 'bitcoin_gbp_response', record: :new_episodes
+  end
+
+  after do 
+    VCR.eject_cassette 
+  end
+
+
+  it 'must have a currency code when initialised' do 
+    bitcoin_gbp_ticker.must_respond_to :currency
+  end
+
+
+end
