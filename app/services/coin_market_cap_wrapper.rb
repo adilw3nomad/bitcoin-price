a53f4c46e749224e9c5713dcb719218c35725db6
adilw3nomad <adiln1993@googlemail.com>
2018-11-09 01:26:27 +0000

    Refactored wrapper to be more extensible by using subclasses

--- app/services/coin_market_cap_wrapper.rb
+++ app/services/coin_market_cap_wrapper.rb
@@ -2,20 +2,17 @@ require 'httparty'
 
 # Wrapper for CoinMarketCap API
 module CoinMarketCapWrapper
-  include HTTParty
-
-  base_uri 'https://api.coinmarketcap.com/v2'
-
-  def self.ticker(id, currency = 'USD')
-    get("/ticker/#{id.strip}?convert=#{currency}")
-  end
-
   class Ticker
+    include HTTParty
+    base_uri 'https://api.coinmarketcap.com/v2'
+
     attr_reader :currency, :crypto_id
-    def initialize(opts)
-      @currency, @crypto_id = opts.values_at(:currency, :crypto_id)
+    def initialize(opts={currency: 'USD'})
+      @currency, @crypto_id = opts.values_at(:currency, :crypto_id).map{|arg| arg.gsub(' ', '')}
     end
 
-    def ticker_data; end
+    def price_info
+      self.class.get("/ticker/#{crypto_id}?convert=#{currency}")
+    end
   end
 end
--- spec/services/ticker_spec.rb
+++ spec/services/ticker_spec.rb
@@ -2,6 +2,17 @@ require_relative '../spec_helper'
 
 describe CoinMarketCapWrapper::Ticker do
   let(:bitcoin_gbp_ticker) { CoinMarketCapWrapper::Ticker.new(crypto_id: '1', currency: 'GBP') }
+  let(:ticker) { CoinMarketCapWrapper::Ticker }
+  
+  describe 'default attributes' do 
+    it 'should include HTTParty methods' do
+      ticker.must_include HTTParty
+    end
+  
+    it 'should have a base uri set to the CoinMarketCap API' do
+      ticker.base_uri.must_equal 'https://api.coinmarketcap.com/v2'
+    end
+  end
 
   describe '.initialize' do
     it 'must have a currency code when initialised' do
@@ -19,11 +30,51 @@ describe CoinMarketCapWrapper::Ticker do
     it 'must have the right cryptocurrency id when initialised' do
       bitcoin_gbp_ticker.crypto_id.must_equal '1'
     end
-  end
 
-  describe '#ticker_data' do 
-    it 'must have a ticker_data method' do 
-      bitcoin_gbp_ticker.must_respond_to :ticker_data
+    # Because we are making building URIs, we can't have spaces in query params
+    it 'must strip any whitespace from arguments' do 
+      ticker_args_have_whitespace = ticker.new(crypto_id: '    1', currency: '   G B P' )
+      ticker_args_have_whitespace.crypto_id.must_equal '1'
+      ticker_args_have_whitespace.currency.must_equal 'GBP'
     end
   end
+
+  describe '#price_info' do
+    it 'should receive a 200 response with a hash of details when called with a valid ID' do
+      VCR.use_cassette('bitcoin_ticker_response', record: :new_episodes) do
+        response = bitcoin_gbp_ticker.price_info
+        response.code.must_equal 200
+        response['data'].must_be_instance_of Hash
+      end
+    end
+  
+
+    it 'should receive a 404 if called with an invalid ID' do
+      VCR.use_cassette('non_existent_ticker_response', record: :new_episodes) do
+        response = ticker.new(crypto_id: '0', currency: 'USD').price_info
+        response.code.must_equal 404
+        response['data'].must_be_nil
+      end
+    end
+
+    it 'should have a currency parameter which defaults to USD' do
+      VCR.use_cassette('bitcoin_ticker_response') do
+        response = ticker.new(crypto_id: '1', currency: 'USD').price_info
+        response['data']['quotes'].keys.must_include 'USD'
+      end
+    end
+
+    it 'should accept a currency code and return the quotes in that currency if available' do
+      VCR.use_cassette('bitcoin_eur_ticker_response') do
+        response = ticker.new(crypto_id: '1', currency: 'EUR').price_info
+        response['data']['quotes'].keys.must_include 'EUR'
+      end
+    end
+  end
+
+  # describe '#ticker_data' do
+  #   it 'must have a ticker_data method' do
+  #     bitcoin_gbp_ticker.must_respond_to :ticker_data
+  #   end
+  # end
 end
